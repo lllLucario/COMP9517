2b7e7e7bb174aa1d20df1177dfd5b4c0ef0e456e
yao27 <55617907+yao27@users.noreply.github.com>
2023-11-15 09:19:36 +0800

    模型叠加 初级

--- utils/project.py
+++ utils/project.py
@@ -1,40 +1,52 @@
+# 导入必要的库
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, BatchNormalization, Dropout
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, BatchNormalization, Dropout, Multiply, Reshape
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.regularizers import l1_l2
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 from elpv_reader import load_dataset
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
+# SE Block definition
+def se_block(input_feature, ratio=16):
+    channel_axis = -1
+    channel = input_feature.shape[channel_axis]
+    se_feature = GlobalAveragePooling2D()(input_feature)
+    se_feature = Reshape((1, 1, channel))(se_feature)
+    se_feature = Dense(channel // ratio, activation='relu')(se_feature)
+    se_feature = Dense(channel, activation='sigmoid')(se_feature)
+    return Multiply()([input_feature, se_feature])
+
 # Parameters
 data_csv_path = 'labels.csv'
 image_directory = './images'
-batch_size = 32
+batch_size = 64
 target_size = (224, 224)
-num_epochs = 75
-num_classes = 4
-learning_rate = 0.00005
+num_epochs = 10
+num_classes = 4  # 修改为四个类别
+learning_rate = 0.0001
 
 # Load dataset
 images, proba, types = load_dataset()
 
 # Map probabilities to class labels
 def map_probability_to_class(prob):
-    if prob == 0:
+    if prob < 0.33:
         return 0
-    elif prob <= 0.33:
+    elif prob < 0.67:
         return 1
-    elif prob <= 0.67:
+    elif prob < 1.0:
         return 2
     else:
         return 3
@@ -42,8 +54,13 @@ def map_probability_to_class(prob):
 # Apply mapping
 probs_mapped = np.array([map_probability_to_class(prob) for prob in proba])
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(images, probs_mapped, test_size=0.25, stratify=probs_mapped, random_state=None)
+# 分割数据集为单晶、多晶和混合
+X_mono, y_mono = images[types == 'mono'], probs_mapped[types == 'mono']
+X_poly, y_poly = images[types == 'poly'], probs_mapped[types == 'poly']
+X_mixed, y_mixed = images, probs_mapped
+
+# Split data for mixed types
+X_train, X_test, y_train, y_test = train_test_split(X_mixed, y_mixed, test_size=0.25, stratify=y_mixed)
 
 # Compute class weights
 sample_weights = compute_sample_weight(class_weight='balanced', y=y_train)
@@ -63,46 +80,27 @@ def preprocess_image(image):
 X_train = np.array([preprocess_image(img) for img in X_train])
 X_test = np.array([preprocess_image(img) for img in X_test])
 
-# Ensure labels are in one-hot encoded format
+# 确保标签是独热编码格式
 y_train = tf.keras.utils.to_categorical(y_train, num_classes)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-# Perform data augmentation
-datagen = ImageDataGenerator(
-    rotation_range=10,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=0.1,
-    zoom_range=0.1,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
-
-# Fit the ImageDataGenerator to the training data
-datagen.fit(X_train)
-
-# Augment the training data by using the ImageDataGenerator
-augmented_data = datagen.flow(X_train, y_train, batch_size=batch_size)
-
-# Combine the original and augmented data
-X_train_augmented = np.concatenate([X_train, augmented_data[0][0]])
-y_train_augmented = np.concatenate([y_train, augmented_data[0][1]])
-
-# Recompute class weights for the combined dataset
-sample_weights_augmented = compute_sample_weight(class_weight='balanced', y=np.argmax(y_train_augmented, axis=1))
-
-
-# Define ResNet50 model with Elastic Net regularization (L1 and L2)
-base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(target_size[0], target_size[1], 3))
+# 定义 ResNet50 模型，只定义一次
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
-x = BatchNormalization()(x)
-x = Dropout(0.7)(x)  # Increase dropout rate to 0.7
-# Add L1-L2 regularization to the Dense layer
 x = Dense(1024, activation='relu', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(x)
 x = BatchNormalization()(x)
-x = Dropout(0.7)(x)  # Increase dropout rate to 0.7
-# Add L1-L2 regularization to the output layer as well
+x = Dropout(0.7)(x)
+predictions = Dense(num_classes, activation='softmax', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# 编译模型，只编译一次
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# 添加 L1-L2 正则化
+x = Dense(1024, activation='relu', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(x)
+x = BatchNormalization()(x)
+x = Dropout(0.7)(x)
 predictions = Dense(num_classes, activation='softmax', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(x)
 
 # Final model
@@ -116,13 +114,7 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), l
 
 # Train model with the checkpoint callback
 try:
-    history = model.fit(
-        X_train_augmented, y_train_augmented,
-        sample_weight=sample_weights_augmented,
-        epochs=num_epochs,
-        validation_data=(X_test, y_test),
-        callbacks=[checkpoint]
-    )
+    history = model.fit(X_train, y_train, epochs=num_epochs, validation_data=(X_test, y_test), sample_weight=sample_weights, callbacks=[checkpoint])
 except Exception as e:
     print('Exception occurred: ', str(e))
 
@@ -161,36 +153,61 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 
 plt.show()
-# Random Forest classifier
-# Feature extraction for Random Forest
-feature_extractor = Model(inputs=base_model.input, outputs=base_model.get_layer('conv5_block3_out').output)
 
-# Extract features
-train_features = feature_extractor.predict(X_train)
-test_features = feature_extractor.predict(X_test)
+# SVM 分类器
+# 交叉验证参数
+resnet_test_predictions = model.predict(X_test)
+resnet_test_classes = np.argmax(resnet_test_predictions, axis=1)
 
-# Reshape features for Random Forest compatibility
-train_features = np.reshape(train_features, (train_features.shape[0], -1))
-test_features = np.reshape(test_features, (test_features.shape[0], -1))
+# 初始化用于保存交叉验证预测的数组
+svm_cv_predictions = np.zeros(len(X_test))
 
-rf_classifier = RandomForestClassifier(
-    n_estimators=200,            # 树的数量
-    max_depth=30,                # 树的最大深度
-    min_samples_split=10,        # 内部节点再划分所需最小样本数
-    min_samples_leaf=8,          # 叶子节点最少样本数
-    max_features='sqrt',         # 寻找最佳分割时考虑的最大特征数
-    bootstrap=False,             # 是否使用bootstrap样本
-    class_weight='balanced',     # 类别权重
-    random_state=80            # 随机种子
-)
+# 交叉验证
+n_splits = 5
+kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+svm_accuracies = []
+svm_cv_predictions = np.zeros(len(X_test))
 
-# 训练随机森林分类器
-rf_classifier.fit(train_features, np.argmax(y_train, axis=1))
+for train_index, test_index in kf.split(X_train):
+    # 分割数据
+    X_train_fold, X_val_fold = X_train[train_index], X_train[test_index]
+    y_train_fold, y_val_fold = y_train[train_index], y_train[test_index]
 
-# 评估随机森林分类器
-rf_predictions = rf_classifier.predict(test_features)
-rf_accuracy = accuracy_score(np.argmax(y_test, axis=1), rf_predictions)
-rf_f1 = f1_score(np.argmax(y_test, axis=1), rf_predictions, average='weighted')
-print(f'Random Forest Confusion Matrix:\n{confusion_matrix(np.argmax(y_test, axis=1), rf_predictions)}')
-print(f'Random Forest Accuracy: {rf_accuracy}')
-print(f'Random Forest F1 Score: {rf_f1}')
+    # 特征提取
+    train_features = feature_extractor.predict(X_train_fold)
+    train_features = np.reshape(train_features, (train_features.shape[0], -1))
+    val_features = feature_extractor.predict(X_val_fold)
+    val_features = np.reshape(val_features, (val_features.shape[0], -1))
+
+    # 训练 SVM 分类器
+    svm_classifier = SVC(kernel='rbf', C=1.0, gamma='auto')
+    svm_classifier.fit(train_features, np.argmax(y_train_fold, axis=1))
+
+    # 在验证集上预测
+    svm_val_predictions = svm_classifier.predict(val_features)
+
+    # 获取一维数组格式的标签
+    y_val_fold_labels = np.argmax(y_val_fold, axis=1)
+
+    # 计算并存储 SVM 准确率
+    svm_accuracy = accuracy_score(y_val_fold_labels, svm_val_predictions)
+
+    svm_accuracies.append(svm_accuracy)
+
+# 创建元数据集
+meta_features = np.vstack([resnet_test_classes, svm_cv_predictions]).T
+
+# 训练逻辑回归元模型
+meta_model = LogisticRegression()
+meta_model.fit(meta_features, np.argmax(y_test, axis=1))
+
+# 对测试集进行最终预测
+final_predictions = meta_model.predict(meta_features)
+
+# 性能评估
+final_accuracy = accuracy_score(np.argmax(y_test, axis=1), final_predictions)
+final_f1 = f1_score(np.argmax(y_test, axis=1), final_predictions, average='weighted')
+
+# 输出平均性能指标
+print(f'Final Accuracy: {final_accuracy}')
+print(f'Final F1 Score: {final_f1}')
